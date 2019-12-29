f18e0c72246fc752f11661963399588b74e5e4ac
Venyige Balázs <45009481+balazske96@users.noreply.github.com>
2019-12-29 23:12:52 +0100

    reload after redirection

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   // This function is just a helper function to avoid react http's problem
   if(window.location.protocol === 'https:'){
       window.location.href = 'http://venyige-weather.herokuapp.com';
+      window.location.reload();
   }
   //Function ends (delete later)
 
