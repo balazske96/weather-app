a8ebf2dadc612cfb4a15549876e188bf445a7219
Balázs <venyigebalazs96@gmail.com>
2020-01-10 11:17:38 +0100

    5 day average data from the 40 hour forecast

--- src/components/DaysContainter.js
+++ src/components/DaysContainter.js
@@ -1,36 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Day from './Day';
 import axios from 'axios';
+import { getAverageForecastFromQuery, timeStampConverterToDay, findHighestOccouranceString } from './../helpers/Helper'
 
 export default function DaysContainer(props) {
     const [days, setDays] = useState([]);
 
-    const timeStampConverterToDay = (timeStamp) => {
-        const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-        let date = new Date(timeStamp * 1000);
-        date.setHours(date.getHours() - 1);
-        return dayArray[date.getDay()];
-
-    }
-
     useEffect(() => {
         axios.get('https://api.openweathermap.org/data/2.5/forecast?q=Budapest,hu&units=metric&APPID=f20aa9b471478b0c833a2bea588f7a05')
             .then(res => {
-                let result = []
-                let counter = 0;
-                for (let i = 7; counter < 5; i += 8) {
-                    console.log(res.data.list[i])
-                    let day = {
-                        type: res.data.list[i].weather[0].icon,
-                        min: Math.floor(res.data.list[i].main.temp_min),
-                        max:  Math.floor(res.data.list[i].main.temp_max),
-                        name: timeStampConverterToDay(res.data.list[i].dt)
-                    }
-                    result.push(day);
-                    counter++;
-                }
-                setDays(result)
-
+                let cloneOfData = JSON.parse(JSON.stringify(res)); //deep clone the result object
+                setDays(getAverageForecastFromQuery(cloneOfData.data.list));
             })
     }, [])
 
@@ -42,7 +22,8 @@ export default function DaysContainer(props) {
                         name={day.name}
                         min={day.min}
                         max={day.max}
-                        type={day.type}></Day>
+                        type={findHighestOccouranceString(day.typeList)}
+                        symbolSpecifier={findHighestOccouranceString(day.symbolSpecifierList)}></Day>
                 })
             }
         </React.Fragment>
